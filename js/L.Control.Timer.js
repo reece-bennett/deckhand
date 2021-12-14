5d56f8b0812d4fb2aaf8ab76f0409fbefdbdcc04
Reece Bennett <reece.bennett1@gmail.com>
2021-12-14 23:02:46 +0000

    Add timer panel

--- js/L.Control.Timer.js
+++ js/L.Control.Timer.js
@@ -1,16 +1,38 @@
 L.Control.Timer = L.Control.extend({
   onAdd: function (map) {
-    const container = L.DomUtil.create('div', 'leaflet-bar')
-    const button = L.DomUtil.create('a', 'material-icons-outlined', container)
-    button.innerText = 'timer'
-    button.href = '#'
-    button.title = 'Timers'
-    button.role = 'button'
+    const container = L.DomUtil.create('div', 'control-timer control-timer--expanded')
+
+    const openButton = L.DomUtil.create('a', 'control-timer__open-button material-icons-outlined', container)
+    openButton.innerText = 'timer'
+    openButton.href = '#'
+    openButton.title = 'Timers'
+    openButton.role = 'button'
+    L.DomEvent.on(openButton, 'click', this._expand, this)
+
+    const panel = L.DomUtil.create('section', 'control-timer__panel', container)
+
+    const title = L.DomUtil.create('div', null, panel)
+    title.innerText = 'Timers'
+
+    const closeButton = L.DomUtil.create('a', 'control-timer__close-button material-icons-outlined', panel)
+    closeButton.innerText = 'close'
+    closeButton.href = '#'
+    closeButton.role = 'button'
+    L.DomEvent.on(closeButton, 'click', this._collapse, this)
+
     return container
   },
 
   onRemove: function (map) {
 
+  },
+
+  _expand: function () {
+    L.DomUtil.addClass(this._container, 'control-timer--expanded')
+  },
+
+  _collapse: function () {
+    L.DomUtil.removeClass(this._container, 'control-timer--expanded')
   }
 })
 
